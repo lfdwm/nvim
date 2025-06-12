6fd3a7b6e9e88486359d653666f652b8213afade
lfdwm <linus.monie@gmail.com>
2025-06-12 23:06:56 +0200

    Cleanup for usecase/system

--- init.lua
+++ init.lua
@@ -24,33 +24,3 @@ require("neo-tree").setup {
 local dap = require("dap")
 dap.listeners.before.event_terminated["dapui_config"] = function() end
 dap.listeners.before.event_exited["dapui_config"]     = function() end
-
--- Default to openapi 3.0 instead of 3.1
--- require("lspconfig").yamlls.setup {
---   settings = {
---     yaml = {
---       schemaStore = {
---         -- You must disable built-in schemaStore support if you want to use
---         -- this plugin and its advanced options like `ignore`.
---         enable = false,
---         -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
---         url = "",
---       },
---       schemas = require("schemastore").yaml.schemas {
---         replace = {
---           ["openapi.json"] = {
---             description = "Open API documentation files",
---             fileMatch = { "openapi.json", "openapi.yml", "openapi.yaml" },
---             name = "openapi.json",
---             url = "https://spec.openapis.org/oas/3.1/schema/2022-10-07",
---             versions = {
---               ["3.0"] = "https://spec.openapis.org/oas/3.0/schema/2021-09-28",
---               ["3.1"] = "https://spec.openapis.org/oas/3.1/schema/2022-10-07"
---             }
---           },
---         },
---       },
---       validate = { enable = true },
---     },
---   },
--- }
--- lua/plugins/mustache.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    "mustache/vim-mustache-handlebars",
-    lazy = false
-  }
-}
